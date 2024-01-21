ecdc17b13ef4422f1b129abc995d0ce0b636c4ea
karan rathor <karanrathor80@gmail.com>
2024-01-21 21:49:35 +0530

    digital

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import './index.css'
 function App() {
+  let time=new Date().toLocaleTimeString();
+  const[etime,setTime]=useState(time);
+  const update=()=>{
+    time=new Date().toLocaleTimeString();
+    setTime(time);
+  }
+  setInterval(update,1000)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <h1>{etime}</h1>
+     
+     
+    </>
   );
 }
 
